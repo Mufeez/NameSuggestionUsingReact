c5b68fa67469f30c0ef229184efccd8f0f76c18a
mufeez <mufeez.ahmed22@gmail.com>
2021-04-18 13:33:20 +0530

    Adding Namecard component and redirection link for domain

---
+++ src/components/ResultsContainer/Namecard/Namecard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Namecard.css";
+
+const namecheapUrl =
+  "https://www.namecheap.com/domains/registration/results/?domain=";
+
+const nameCard = ({ suggestedName }) => {
+  return (
+    <a
+      className="name-card-link"
+      href={`${namecheapUrl}${suggestedName}`}
+      target="_blank"
+    >
+      <div className="result-name-card">
+        <p className="result-name">{suggestedName}</p>
+      </div>
+    </a>
+  );
+};
+
+export default nameCard;
--- src/components/ResultsContainer/ResultsContainer.jsx
+++ src/components/ResultsContainer/ResultsContainer.jsx
@@ -1,14 +1,12 @@
 import React from "react";
+import "./ResultsContainer.css";
+import Name from "./Namecard/Namecard";
 
 const resultContainer = ({ suggestedNames }) => {
   const suggestedNamesList = suggestedNames.map((suggestedName) => {
-    return <p>{suggestedName}</p>;
+    return <Name key={suggestedName} suggestedName={suggestedName} />;
   });
-  return (
-    <div className="results-container">
-      <p>{suggestedNamesList}</p>
-    </div>
-  );
+  return <div className="results-container">{suggestedNamesList}</div>;
 };
 
 export default resultContainer;
