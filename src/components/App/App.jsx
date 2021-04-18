c84aaf8af80977692a9421d4861d5280c242cd2e
mufeez <mufeez.ahmed22@gmail.com>
2021-04-18 11:31:56 +0530

    using Header and Search component inside app component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,39 @@
 import react from "react";
 import "./App.css";
 import Header from "./../Header/Header";
+import SearchBox from "./../Search/Search";
 
 class App extends react.Component {
   state = {
     fastingState: "I am Fasting",
-    HeaderText: "This is new ,Name it!",
+    headerText: "This is new ,Name it!",
+    headerExpanded: true,
+  };
+
+  handleSearchInputChange = (inputText) => {
+    this.setState({ headerExpanded: !inputText });
+    console.log("input Changed to" + inputText);
   };
   render() {
     return (
       <div>
-        <Header headTitle={this.state.HeaderText}></Header>
-        <h3>{this.state.fastingState}</h3>
-        <button
-          onClick={() => {
-            this.setState({
-              fastingState: "Yeah ! Completed the Fasting!",
-            });
-          }}
-        >
-          Iftaar
-        </button>
+        <Header
+          headerExpanded={this.state.headerExpanded}
+          headTitle={this.state.headerText}
+        />
+        <SearchBox onInputChange={this.handleSearchInputChange} />
+        <div className="button-header3-container">
+          <h3>{this.state.fastingState}</h3>
+          <button
+            onClick={() => {
+              this.setState({
+                fastingState: "Yeah ! Completed the Fasting!",
+              });
+            }}
+          >
+            Iftaar
+          </button>
+        </div>
       </div>
     );
   }
